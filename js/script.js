7e4e6299edd038af2d38b476212acdef490cdecf
Fahim Ahmed Ayon <fa369383@gmail.com>
2025-08-29 22:44:15 +0600

    before histry

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 // funtion of coin
-function balanceCutter(id){
+function balanceCutter(id,id2,id3){
       const callButton = document.getElementById(id).addEventListener('click',function(e){
     e.preventDefault();
     // console.log('clicked')
 
-    
+    const number = document.getElementById(id2).innerText
+    // console.log(number)
+    const name = document.getElementById(id3).innerText
 let coinValue = parseInt(document.getElementById("coin-counter").innerText)
 
     if(coinValue<20){
@@ -12,11 +14,11 @@ let coinValue = parseInt(document.getElementById("coin-counter").innerText)
       return;
     }
     else{
-      alert("calling")
+      alert("📞 calling "+ number + " \n"+ name)
     }
     coinValue-=20
 let  newValue = coinValue;
-console.log(newValue)
+// console.log(newValue)
 document.getElementById("coin-counter").innerText = newValue;
 
 })
@@ -45,10 +47,34 @@ document.getElementById(id1).addEventListener("click", function () {
   const numberElement = document.getElementById(id2);
   const number = numberElement.innerText.trim();
 
-  navigator.clipboard.writeText(number)
+    navigator.clipboard.writeText(number)
+    .then(() => {
+      alert("Number copied to clipboard!");
+    })
+    .catch(err => {
+      console.error("Failed to copy:", err);
+    });
+      navigator.clipboard.writeText(number)
+
+    let copyVal = parseInt(document.getElementById("copy-cnt").innerText)
+        console.log(copyVal)
+
+      copyVal+=1
+     let copyNewVal = copyVal;
+    document.getElementById("copy-cnt").innerText = copyNewVal;
+
   
 });
 }
+balanceCutter("call-button1","phn-number1","all1")
+balanceCutter("call-button2","phn-number2","all2")
+balanceCutter("call-button3","phn-number3","all3")
+balanceCutter("call-button4","phn-number4","all4")
+balanceCutter("call-button5","phn-number5","all5")
+balanceCutter("call-button6","phn-number6","all6")
+balanceCutter("call-button7","phn-number7","all7")
+balanceCutter("call-button8","phn-number8","all8")
+balanceCutter("call-button9","phn-number9","all9")
 
 heartCount("heart1")
 heartCount("heart2")
@@ -70,12 +96,4 @@ copyCardNumber("copy-button7","phn-number7")
 copyCardNumber("copy-button8","phn-number8")
 copyCardNumber("copy-button9","phn-number9")
 
-balanceCutter("call-button1")
-balanceCutter("call-button2")
-balanceCutter("call-button3")
-balanceCutter("call-button4")
-balanceCutter("call-button5")
-balanceCutter("call-button6")
-balanceCutter("call-button7")
-balanceCutter("call-button8")
-balanceCutter("call-button9")
+
